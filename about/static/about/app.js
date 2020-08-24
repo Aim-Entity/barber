e05831e4e4b1dd711b912501c142dbf37a7dc75a
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2020-08-24 16:19:15 +0100

    Phone view about page done

--- about/static/about/app.js
+++ about/static/about/app.js
@@ -1,30 +1,5 @@
 function main() {
-  orangeButton();
   blueButton();
-}
-
-function orangeButton() {
-  if (window.innerWidth > 960) {
-    let btn = document.querySelectorAll(".orange-btn");
-
-    for (i = 0; i < btn.length; i++) {
-      btn[i].addEventListener("mouseover", (e) => {
-        anime({
-          targets: e.target,
-          background: ["rgb(255,255,255)", "rgb(255,121,63)"],
-          color: ["rgb(255,121,63)", "rgb(255,255,255)"],
-        });
-      });
-
-      btn[i].addEventListener("mouseout", (e) => {
-        anime({
-          targets: e.target,
-          background: ["rgb(255,121,63)", "rgb(255,255,255)"],
-          color: ["rgb(255,255,255)", "rgb(255,121,63)"],
-        });
-      });
-    }
-  }
 }
 
 function blueButton() {
